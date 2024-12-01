266788e254c1c66e75f97e95d091dee2e2f4433d
Bert Riffelmacher <sotsoguk@gmail.com>
2024-12-01 13:53:48 +0100

    Day01 Solved in Python

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# adventOfCode2024
+![](https://img.shields.io/badge/stars%20⭐-19-yellow)
+![](https://img.shields.io/badge/day%20📅-12-blue) 
+![](https://img.shields.io/badge/days%20completed-09-red)
+
+# Advent of Code 2024
+
+This repo contains the 2024 [Advent Of Code 2024](https://adventofcode.com/2024) solutions.
+Still not finished
+
+## Problems
+
+| Day | Problem | Solution |
+| ---:| ----:   | ----:    |
+| 01  |[Historian Hysteria](https://adventofcode.com/2024/day/1)| [Python](https://github.com/sotsoguk/adventOfCode2024/blob/main/python/day01/day01.py), |
---
+++ python/day01/day01.py
@@ -0,0 +1,49 @@
+from collections import Counter
+import os
+import time
+import heapq
+
+
+def main():
+
+    # input
+    print(os.getcwd())
+    day = "01"
+    year = "204"
+    input_file = f"./inputs/day{day}.txt"
+    print(input_file)
+    part1, part2 = 0, 0
+    leftList, rightList = [], []
+
+    with open(input_file) as f:
+        # groups = f.read().split('\n')
+        lines = f.read().splitlines()
+
+    for l in lines:
+        a, b = list(map(int, l.split("   ")))
+        leftList.append(a)
+        rightList.append(b)
+
+    counterLeft, counterRight = Counter(leftList), Counter(rightList)
+
+    # calc
+    start_time = time.time()
+    part1 = sum(
+        map(
+            lambda tuple: abs(tuple[0] - tuple[1]),
+            zip(sorted(leftList), sorted(rightList)),
+        )
+    )
+
+    part2 = sum([counterLeft[el] * el * counterRight[el] for el in counterLeft])
+
+    # output
+    duration = int((time.time() - start_time) * 1000000)
+    header = "#" * 20
+    print(
+        f"{header}\n*AoC {year} - Day {day} *\n{header}\n\nPart 1:\t{part1}\nPart 2:\t{part2}\nTime:\t{duration // 1000} ms"
+    )
+
+
+if __name__ == "__main__":
+    main()
