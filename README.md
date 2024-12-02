99ed76b072bb9b81cbea661f7736eae0ecf4d88c
Bert Riffelmacher <sotsoguk@gmail.com>
2024-12-02 15:03:24 +0100

    day02

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-![](https://img.shields.io/badge/stars%20⭐-02-yellow)
-![](https://img.shields.io/badge/day%20📅-01-blue) 
-![](https://img.shields.io/badge/days%20completed-01-red)
+![](https://img.shields.io/badge/stars%20⭐-04-yellow)
+![](https://img.shields.io/badge/day%20📅-02-blue) 
+![](https://img.shields.io/badge/days%20completed-02-red)
 
 # Advent of Code 2024
 
@@ -9,6 +9,7 @@ Still not finished
 
 ## Problems
 
-| Day | Problem | Solution |
-| ---:| ----:   | ----:    |
-| 01  |[Historian Hysteria](https://adventofcode.com/2024/day/1)| [Python](https://github.com/sotsoguk/adventOfCode2024/blob/main/python/day01/day01.py), |
+| Day |                                                   Problem |                                                                                Solution |
+| --: | --------------------------------------------------------: | --------------------------------------------------------------------------------------: |
+|  01 | [Historian Hysteria](https://adventofcode.com/2024/day/1) | [Python](https://github.com/sotsoguk/adventOfCode2024/blob/main/python/day01/day01.py), |
+|  02 |  [Red-Nosed Reports](https://adventofcode.com/2024/day/2) |  [Python](https://github.com/sotsoguk/adventOfCode2024/blob/main/python/day02/day02.py) |
---
+++ python/day02/day02.py
@@ -0,0 +1,60 @@
+from collections import Counter
+from itertools import compress
+import os
+import time
+import heapq
+
+
+def safe(level):
+    firstDiff = [t - s for s, t in zip(level, level[1:])]
+    return (
+        all([y <= -1 for y in firstDiff]) and all([y >= -3 for y in firstDiff])
+    ) or (all([y >= 1 for y in firstDiff]) and all([y <= 3 for y in firstDiff]))
+
+
+def safe2(level, s=0):
+    return any(safe(level[:i] + level[i + 1 :]) for i in range(len(level)))
+
+
+def main():
+
+    # input
+    print(os.getcwd())
+    day = "02"
+    year = "2024"
+    input_file = f"./inputs/day{day}.txt"
+    print(input_file)
+    part1, part2 = 0, 0
+    levels = []
+
+    with open(input_file) as f:
+        # groups = f.read().split('\n')
+        lines = f.read().splitlines()
+
+    for l in lines:
+        level = list(map(int, l.split()))
+        levels.append(level)
+
+    # calc
+    start_time = time.time()
+
+    # part1
+    safeList1 = list(map(safe, levels))
+    part1 = sum(safeList1)
+    # only process unsafe lists in part2
+    unsafeList1 = list(compress(levels, [not elem for elem in safeList1])) 
+
+    # part2
+    p2list = list(map(safe2, unsafeList1))
+    part2 = sum(p2list) + part1
+
+    # output
+    duration = int((time.time() - start_time) * 1000000)
+    header = "#" * 20
+    print(
+        f"{header}\n*AoC {year} - Day {day} *\n{header}\n\nPart 1:\t{part1}\nPart 2:\t{part2}\nTime:\t{duration // 1000} ms"
+    )
+
+
+if __name__ == "__main__":
+    main()
